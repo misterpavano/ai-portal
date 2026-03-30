8ac74478335d37fa216a59b2355c5688e977cdcc
Wally <hilltop_rhizome5c@icloud.com>
2026-03-30 10:11:20 -0400

    Route Assistant: no doc type selected by default

--- src/atoms/routeValidatorAtom.ts
+++ src/atoms/routeValidatorAtom.ts
@@ -7,7 +7,7 @@ export const routeValidatorStepAtom = atom<RouteValidatorStep>({
 });
 
 const initialRouteValidatorValues: RouteValidatorFlow = {
-  documentType: "route",
+  documentType: "",
   file: {
     fileId: "",
     fileName: "",
